4cd5d797ceb4dc4967494ce0235e44fde37c5276
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-07-14 12:52:50 +0200

    Create before blocks for readers and writers

--- src/test/java/org/gurpsdomain/integration/PipelineTest.java
+++ src/test/java/org/gurpsdomain/integration/PipelineTest.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 import org.gurpsdomain.Pipeline;
 import org.gurpsdomain.adapters.input.SheetInput;
 import org.gurpsdomain.adapters.output.SheetOutput;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -18,21 +19,34 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PipelineTest {
+    private Reader reader;
+    private Writer writer;
+
+    @Before
+    public void createReader() throws FileNotFoundException {
+        reader = new FileReader(new File("src/test/resources/sheets/dai-blackthorn.yml"));
+    }
+
+    @Before
+    public void createWriter() {
+        writer = new StringWriter();
+    }
+
     @Test
     public void shouldProduceCorrectJson() throws FileNotFoundException {
-        Reader reader = new FileReader(new File("src/test/resources/sheets/dai-blackthorn.yml"));
         SheetInput input = fromYaml(reader);
-        Writer writer = new StringWriter();
         SheetOutput output = toJson(writer);
 
         Pipeline.flow(input).into(output);
 
+        in(outputAsMap(writer)).path("points.earned").shouldEqual(250.0);
+    }
+
+    private Map<String, Object> outputAsMap(Writer writer) {
         String result = writer.toString();
         Gson gson = new Gson();
         Type stringObjectMap = new TypeToken<Map<String, Object>>(){}.getType();
-        Map<String, Object> data = gson.fromJson(result, stringObjectMap);
-
-        in(data).path("points.earned").shouldEqual(250.0);
+        return gson.fromJson(result, stringObjectMap);
     }
 }
 
