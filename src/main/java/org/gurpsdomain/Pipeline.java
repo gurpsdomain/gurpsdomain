b3cec0b769741807b5cb6e37269776b84dd28e45
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-07-14 11:10:07 +0200

    Only keep sheet

--- src/main/java/org/gurpsdomain/Pipeline.java
+++ src/main/java/org/gurpsdomain/Pipeline.java
@@ -9,26 +9,20 @@ import java.io.Writer;
 import java.util.Map;
 
 public class Pipeline {
-
     public static Pipeline flow(Reader reader) {
         return new Pipeline(reader);
     }
 
-    private final Reader reader;
-    private final Map<String, Object> data;
+    private final Sheet sheet;
 
     private Pipeline(Reader reader) {
-        this.reader = reader;
         Yaml yaml = new Yaml();
         Map<String, Object> data = (Map<String, Object>)yaml.load(reader);
-        this.data = data;
-
+        this.sheet = new Sheet((Integer) data.get("basepoints"));
     }
 
     public void into(Writer writer) {
         Gson gson = new Gson();
-        Sheet sheet = new Sheet((Integer)data.get("basepoints"));
         gson.toJson(sheet, writer);
-
     }
 }
