22116e1d22011eca570d6c5d1a22b980ba9f6cc5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-07-22 15:29:56 +0200

    Start a ReflectionReader

---
+++ src/test/java/org/gurpsdomain/adapters/output/converter/ReflectionReaderTest.java
@@ -0,0 +1,41 @@
+package org.gurpsdomain.adapters.output.converter;
+
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.gurpsdomain.adapters.output.converter.ReflectionReader.read;
+
+public class ReflectionReaderTest {
+    private static final int ANY_INT_VALUE = 37;
+
+    @Test
+    public void shouldReadProperties() throws NoSuchFieldException, IllegalAccessException {
+        SingleValue singleValue = new SingleValue(ANY_INT_VALUE);
+
+        Integer value = read("value").from(singleValue);
+
+        assertThat(value, is(ANY_INT_VALUE));
+    }
+}
+
+class ReflectionReader {
+    public static ReflectionReader read(String property) {
+        return new ReflectionReader(property);
+    }
+
+    private String property;
+
+    private ReflectionReader(String property) {
+        this.property = property;
+    }
+
+
+    public <T> T from(Object object) throws NoSuchFieldException, IllegalAccessException {
+        Class<?> objectClass = object.getClass();
+        Field field = objectClass.getDeclaredField(property);
+        return (T) field.get(object);
+    }
+}
