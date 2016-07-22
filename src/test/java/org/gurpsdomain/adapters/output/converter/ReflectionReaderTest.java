de714d09ac75b561a8c213cff122ec2014c03fd0
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-07-22 16:00:19 +0200

    Move ReflectionReader to own file

--- src/test/java/org/gurpsdomain/adapters/output/converter/ReflectionReaderTest.java
+++ src/test/java/org/gurpsdomain/adapters/output/converter/ReflectionReaderTest.java
@@ -2,11 +2,9 @@ package org.gurpsdomain.adapters.output.converter;
 
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-
+import static org.gurpsdomain.adapters.output.converter.ReflectionReader.read;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.gurpsdomain.adapters.output.converter.ReflectionReader.read;
 
 public class ReflectionReaderTest {
     private static final int ANY_INT_VALUE = 37;
@@ -27,42 +25,6 @@ public class ReflectionReaderTest {
         Integer value = read("singleValue", "value").from(nestedValue);
 
         assertThat(value, is(ANY_INT_VALUE));
-    }
-}
-
-class ReflectionReader {
-    public static ReflectionReader read(String... properties) {
-        return new ReflectionReader(properties);
-    }
-
-    private String[] properties;
-
-    private ReflectionReader(String... properties) {
-        this.properties = properties;
-    }
-
-
-    public <T> T from(Object object) {
-        Object current = object;
-        for (String property: properties) {
-            current = safeReadFrom(property, current);
-        }
-        return (T) current;
-    }
-
-    private <T> T safeReadFrom(String property, Object object) {
-        try {
-            return unsafeReadFrom(property, object);
-        } catch (IllegalAccessException|NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private <T> T unsafeReadFrom(String property, Object object) throws NoSuchFieldException, IllegalAccessException {
-        Class<?> objectClass = object.getClass();
-        Field field = objectClass.getDeclaredField(property);
-        field.setAccessible(true);
-        return (T) field.get(object);
     }
 }
 
