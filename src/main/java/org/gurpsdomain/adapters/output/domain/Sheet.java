b6ced03a35758e82bb60ba299c447458054571ef
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-27 09:31:21 +0200

    Include advantages in output json

--- src/main/java/org/gurpsdomain/adapters/output/domain/Sheet.java
+++ src/main/java/org/gurpsdomain/adapters/output/domain/Sheet.java
@@ -1,7 +1,11 @@
 package org.gurpsdomain.adapters.output.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sheet {
     private final Points points;
+    private final List<Object> advantages = new ArrayList<Object>();
 
     public Sheet(Points points) {
         this.points = points;
