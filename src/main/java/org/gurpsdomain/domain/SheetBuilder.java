fc8d9085f45d2e3e0d0d853c5f15bfa09442e4f8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-31 18:47:56 +0200

    Seperate value from keys in metadata

--- src/main/java/org/gurpsdomain/domain/SheetBuilder.java
+++ src/main/java/org/gurpsdomain/domain/SheetBuilder.java
@@ -1,5 +1,7 @@
 package org.gurpsdomain.domain;
 
+import static org.gurpsdomain.domain.Util.slice;
+
 public class SheetBuilder {
     public static SheetBuilder builder() {
         return new SheetBuilder();
@@ -18,10 +20,22 @@ public class SheetBuilder {
     public  void addAdvantage(Advantage advantage) { sheet.addAdvantage(advantage);}
 
     public  void addMetaData(String... keys) {
-        sheet.setMetaDataProperty(keys[0], keys[1]);
+        String value = keys[keys.length - 1];
+        keys = slice(keys, 0, keys.length - 1);
+        sheet.setMetaDataProperty(keys[0], value);
     }
 
     public Sheet build() {
         return sheet;
+    }
+}
+
+class Util {
+    public static String[] slice(String[] original, int startInclusive, int finishExclusive) {
+        String[] slice = new String[finishExclusive - startInclusive];
+        for (int index = startInclusive; index < finishExclusive; index++) {
+            slice[index - startInclusive] = original[index];
+        }
+        return slice;
     }
 }
