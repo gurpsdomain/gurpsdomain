fe05fb2dbc78729171d85a1c822c29226e16a4e5
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2016-09-13 19:49:12 +0200

    Fixed tests

--- src/main/java/org/gurpsdomain/domain/description/AdvantageDescription.java
+++ src/main/java/org/gurpsdomain/domain/description/AdvantageDescription.java
@@ -1,21 +1,30 @@
 package org.gurpsdomain.domain.description;
 
 import org.gurpsdomain.domain.Advantage;
+import org.gurpsdomain.domain.Modifier;
 import org.gurpsdomain.domain.PageReference;
+
+import java.util.List;
 
 public class AdvantageDescription implements PageReference {
     public String name;
     public String pageReference;
     public int cost;
+    public List <Modifier> modifiers;
 
     public AdvantageDescription(String name, int cost, String pageReference) {
+        this(name, cost, pageReference, null);
+    }
+
+    public AdvantageDescription(String name, int cost, String pageReference, List<Modifier> modifiers) {
         this.name = name;
         this.cost = cost;
         this.pageReference = pageReference;
+        this.modifiers = null;
     }
 
     public Advantage createAdvantage() {
-        return new Advantage(name, cost, pageReference);
+        return new Advantage(name, cost, pageReference, modifiers);
     }
 
     @Override
