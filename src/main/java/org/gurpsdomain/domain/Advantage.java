8a9785217794c69b1ab3083eb70c0c16621de7ff
Marcel Gorissen <damnhighc4@hotmail.com>
2016-11-09 10:54:02 +0100

    small changes in Advantage class. Introduction of unmodifiedCost versus (modified) cost

--- src/main/java/org/gurpsdomain/domain/Advantage.java
+++ src/main/java/org/gurpsdomain/domain/Advantage.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Advantage implements PageReference {
     public String name;
     public String pageReference;
+    private int unmodifiedCost;
     public int cost;
     public List<Modifier> modifiers;
 
@@ -13,14 +14,18 @@ public class Advantage implements PageReference {
         this(name, cost, pageReference, new ArrayList<Modifier>());
     }
 
-    public Advantage(String name, int cost, String pageReference, List<Modifier> modifiers) {
+    public Advantage(String name, int unmodifiedCost, String pageReference, List<Modifier> modifiers) {
         this.name = name;
-        this.cost = cost;
+        this.unmodifiedCost = unmodifiedCost;
         this.pageReference = pageReference;
         this.modifiers = modifiers;
     }
 
-    private int getCost(){return cost;}
+    private int getCost(){
+        //TODO determine actual cost by iterating over the active modifiers and calculate modified cost
+        this.cost = this.unmodifiedCost;
+        return this.cost;
+    }
 
     public void payCost(Points points) {
         points.addAdvantage(getCost());
