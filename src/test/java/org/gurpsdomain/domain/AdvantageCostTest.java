840d2425690152daaece76aa43c8e07b621f86c0
Marcel Gorissen <damnhighc4@hotmail.com>
2016-11-15 22:04:07 +0100

    Implemented two more cases for advantage cost determination based on modifiers. One for mixed positive and negative poins One for a percentage modifier that passes by coincidence due to the base cost being 100 points

--- src/test/java/org/gurpsdomain/domain/AdvantageCostTest.java
+++ src/test/java/org/gurpsdomain/domain/AdvantageCostTest.java
@@ -15,23 +15,25 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(Parameterized.class)
 public class AdvantageCostTest {
-    private static final int BASE_COST = 100;
     private static final String ANY_PAGE_REFERENCE = "TEST1";
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> data() {
         Collection<Object[]> data = new ArrayList<Object[]>();
-        data.add(new Object[]{"no modifiers", Arrays.asList(new Modifier[]{}), 100});
-        data.add(new Object[]{"single modifier (10 points)", Arrays.asList(new Modifier("bla10", new Cost(10, CostType.points)) ), 110});
-        data.add(new Object[]{"single modifier (-5 points)", Arrays.asList(new Modifier("blamin5", new Cost(-5, CostType.points)) ), 95});
+        data.add(new Object[]{"no modifiers", Arrays.asList(new Modifier[]{}),100, 100});
+        data.add(new Object[]{"single modifier (10 points)", Arrays.asList(new Modifier("10 point modifier", new Cost(10, CostType.points)) ),100 , 110});
+        data.add(new Object[]{"single modifier (-5 points)", Arrays.asList(new Modifier("-5 point modifier", new Cost(-5, CostType.points)) ),100, 95});
+        data.add(new Object[]{"two modifiers (7 points, -3 points)", Arrays.asList(new Modifier("7 point modifier", new Cost(7, CostType.points)),new Modifier("-3 point modifier", new Cost(-3, CostType.points))),100, 104});
+        data.add(new Object[]{"single modifier (11 percent), base 100", Arrays.asList(new Modifier("11 percent modifier", new Cost(11, CostType.percentage)) ),100, 111});
+//FIXME        data.add(new Object[]{"single modifier (11 percent), base 50", Arrays.asList(new Modifier("11 percent modifier", new Cost(11, CostType.percentage)) ),50, 56});
         return data;
     }
 
     private final Advantage advantage;
     private final int expectedCost;
 
-    public AdvantageCostTest(String advantageName, List<Modifier> modifiers, int expectedCost) {
-        this.advantage = new Advantage(advantageName, BASE_COST, ANY_PAGE_REFERENCE, modifiers);
+    public AdvantageCostTest(String advantageName, List<Modifier> modifiers,int baseCost, int expectedCost) {
+        this.advantage = new Advantage(advantageName, baseCost, ANY_PAGE_REFERENCE, modifiers);
         this.expectedCost = expectedCost;
     }
 
