106281a8d72c2f465d286ef206e117eaaff07467
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-11-16 15:05:35 +0100

    Apply TestCase Builder pattern

--- src/test/java/org/gurpsdomain/domain/AdvantageCostTest.java
+++ src/test/java/org/gurpsdomain/domain/AdvantageCostTest.java
@@ -1,46 +1,46 @@
 package org.gurpsdomain.domain;
 
+import javafx.scene.effect.Reflection;
+import org.gurpsdomain.adapters.output.converter.ReflectionReader;
 import org.gurpsdomain.domain.description.CostType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
+import static org.gurpsdomain.adapters.output.converter.ReflectionReader.read;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import static org.gurpsdomain.domain.AdvantageCostTestCase.anAdvantage;
+
 @RunWith(Parameterized.class)
 public class AdvantageCostTest {
-    private static final String ANY_PAGE_REFERENCE = "TEST1";
-
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> data() {
         Collection<Object[]> data = new ArrayList<Object[]>();
-        data.add(new Object[]{"no modifiers", Arrays.asList(new Modifier[]{}),100, 100});
-        data.add(new Object[]{"single modifier (10 points)", Arrays.asList(new Modifier("10 point modifier", new Cost(10, CostType.points)) ),100 , 110});
-        data.add(new Object[]{"single modifier (-5 points)", Arrays.asList(new Modifier("-5 point modifier", new Cost(-5, CostType.points)) ),100, 95});
-        data.add(new Object[]{"two modifiers (7 points, -3 points)", Arrays.asList(new Modifier("7 point modifier", new Cost(7, CostType.points)),new Modifier("-3 point modifier", new Cost(-3, CostType.points))),100, 104});
-        data.add(new Object[]{"single modifier (11 percent), base 100", Arrays.asList(new Modifier("11 percent modifier", new Cost(11, CostType.percentage)) ),100, 111});
-        data.add(new Object[]{"single modifier (11 percent), base 50", Arrays.asList(new Modifier("11 percent modifier", new Cost(11, CostType.percentage)) ),50, 56}); //rounding of point costs is always up. See B101
-        data.add(new Object[]{"single modifier (-11 percent), base 50", Arrays.asList(new Modifier("-11 percent modifier", new Cost(-11, CostType.percentage)) ),50, 45}); //rounding of point costs is always up. See B101
-        data.add(new Object[]{"two modifier (11 percent, 33 percent), base 50", Arrays.asList(new Modifier("11 percent modifier", new Cost(11, CostType.percentage)),new Modifier("33 percent modifier", new Cost(33, CostType.percentage)) ),50, 72}); //rounding of point costs is always up. See B101
-        data.add(new Object[]{"single modifier (95 percent), base 100", Arrays.asList(new Modifier("95 percent modifier", new Cost(95, CostType.percentage)) ),100, 195});//No upper bound for posititive percentages
-        data.add(new Object[]{"single modifier (-95 percent), base 100", Arrays.asList(new Modifier("-95 percent modifier", new Cost(-95, CostType.percentage)) ),100, 20}); //Cost reduction by negative percentages is capped at -80%
-        data.add(new Object[]{"mixed modifiers (12 percent, 10 points), base 100", Arrays.asList(new Modifier("12 percent modifier", new Cost(12, CostType.percentage)),new Modifier("10 point modifier", new Cost(10, CostType.points)) ),100, 124});
-        data.add(new Object[]{"mixed modifiers (-40 percent,-10 percent, 5 points,  15 points), base 100", Arrays.asList(new Modifier("-40 percent modifier", new Cost(-40, CostType.percentage)),new Modifier("-10 percent modifier", new Cost(-10, CostType.percentage)),new Modifier("5 point modifier", new Cost(5, CostType.points)),new Modifier("15 point modifier", new Cost(15, CostType.points)) ),100, 60});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).hasExpectedCost(100)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPointModifier(10).hasExpectedCost(110)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPointModifier(-5).hasExpectedCost(95)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPointModifier(7).withPointModifier(-3).hasExpectedCost(104)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPercentageModifier(11).hasExpectedCost(111)});
+        data.add(new Object[]{anAdvantage().withBaseCost(50).withPercentageModifier(11).hasExpectedCost(56)});
+        data.add(new Object[]{anAdvantage().withBaseCost(50).withPercentageModifier(-11).hasExpectedCost(45)});
+        data.add(new Object[]{anAdvantage().withBaseCost(50).withPercentageModifier(11).withPercentageModifier(33).hasExpectedCost(72)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPercentageModifier(95).hasExpectedCost(195)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPercentageModifier(-95).hasExpectedCost(20)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPercentageModifier(12).withPointModifier(10).hasExpectedCost(124)});
+        data.add(new Object[]{anAdvantage().withBaseCost(100).withPercentageModifier(-40).withPercentageModifier(-10).withPointModifier(5).withPointModifier(15).hasExpectedCost(60)});
         return data;
     }
 
     private final Advantage advantage;
     private final int expectedCost;
 
-    public AdvantageCostTest(String advantageName, List<Modifier> modifiers,int baseCost, int expectedCost) {
-        this.advantage = new Advantage(advantageName, baseCost, ANY_PAGE_REFERENCE, modifiers);
-        this.expectedCost = expectedCost;
+    public AdvantageCostTest(AdvantageCostTestCase testCase) {
+        this.advantage = testCase.create();
+        this.expectedCost = testCase.expectedCost;
     }
 
     @Test
@@ -50,5 +50,63 @@ public class AdvantageCostTest {
         advantage.payCost(points);
 
         verify(points).addAdvantage(expectedCost);
+    }
+}
+
+class AdvantageCostTestCase {
+    private static final String ANY_PAGE_REFERENCE = "TEST1";
+
+    public static AdvantageCostTestCase anAdvantage() {
+        return new AdvantageCostTestCase();
+    }
+
+    private int baseCost = 100;
+    private List<Modifier> modifiers = new ArrayList<Modifier>();
+    public int expectedCost = 100;
+
+    private AdvantageCostTestCase(){};
+
+    public AdvantageCostTestCase withBaseCost(int baseCost) {
+        this.baseCost = baseCost;
+        return this;
+    }
+
+    public AdvantageCostTestCase withPointModifier(int value) {
+        this.modifiers.add(new Modifier(String.format("%d point modifier", value), new Cost(value, CostType.points)));
+        return this;
+    }
+
+    public AdvantageCostTestCase withPercentageModifier(int value) {
+        this.modifiers.add(new Modifier(String.format("%d percent modifier", value), new Cost(value, CostType.percentage)));
+        return this;
+    }
+
+    public AdvantageCostTestCase hasExpectedCost(int expectedCost) {
+        this.expectedCost = expectedCost;
+        return this;
+    }
+
+    public Advantage create() {
+        return new Advantage(this.toString(), baseCost, ANY_PAGE_REFERENCE, modifiers);
+    }
+
+    @Override
+    public String toString() {
+        ReflectionReader readValue = read("cost", "value");
+        ReflectionReader readCostType = read("cost", "type");
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("base cost %d", baseCost));
+        builder.append(" ");
+        builder.append(String.format("expected cost %d", expectedCost));
+        builder.append(" ");
+        StringJoiner joiner = new StringJoiner(", ", "(", ")");
+        for (Modifier modifier: modifiers) {
+            int value = readValue.from(modifier);
+            CostType costType = readCostType.from(modifier);
+            joiner.add(String.format("%d%s", value, costType.equals(CostType.percentage)? "%": ""));
+        }
+        builder.append(joiner);
+
+        return builder.toString();
     }
 }
