a96748429707666d60513ca0ebca6341d7e26526
Marcel Gorissen <damnhighc4@hotmail.com>
2016-11-23 20:53:44 +0100

    refactored the ReflectionConverter: breaking up in smaller methods

--- src/main/java/org/gurpsdomain/adapters/output/converter/ReflectionConverter.java
+++ src/main/java/org/gurpsdomain/adapters/output/converter/ReflectionConverter.java
@@ -16,26 +16,40 @@ import static org.gurpsdomain.adapters.output.converter.ReflectionCaller.call;
 import static org.gurpsdomain.adapters.output.converter.ReflectionReader.read;
 
 public class ReflectionConverter implements SheetConverter {
+    private ReflectionReader readName = read("name");
+    private ReflectionReader readCost = read("cost");
+    private ReflectionReader readPageReference = read("pageReference");
+    private ReflectionReader readModifiers = read("modifiers");
+    private ReflectionReader readValue = read("value");
+    private ReflectionReader readType = read("type");
+    private ReflectionReader readControllingAttribute = read("controllingAttribute");
+    private ReflectionReader readDifficultyLevel = read("difficultyLevel");
+    private ReflectionReader readAdvantages = read("advantages");
+    private ReflectionReader readSkills = read("skills");
+    private ReflectionReader readMetaData = read("metaData");
+    private ReflectionReader readNote = read("note");
+    private ReflectionCaller callCost = call("cost");
+    private Sheet sheet;
+
+
     @Override
     public SheetSheet convert(Sheet sheet) {
-        Map<String, String> metaData;
-        metaData = read("metaData").from(sheet);
-        String note = read("note").from(sheet);
+        this.sheet = sheet;
+        return new SheetSheet(convertMetaData(), convertSheetPoints(), convertSheetAdvantages(), convertSheetSkills(), convertNote());
+    }
 
-        SheetPoints sheetPoints = new SheetPoints(read("points", "total").from(sheet), read("points", "advantages").from(sheet), read("points", "skills").from(sheet));
+    private Map<String, String> convertMetaData() {
+        return readMetaData.from(sheet);
+    }
+
+    private SheetPoints convertSheetPoints() {
+        return new SheetPoints(read("points", "total").from(sheet), read("points", "advantages").from(sheet), read("points", "skills").from(sheet));
+    }
+
+    private List<SheetAdvantage> convertSheetAdvantages() {
         List<SheetAdvantage> sheetAdvantages = new ArrayList<>();
-        List<Advantage> domainAdvantages = read("advantages").from(sheet);
-        List<SheetSkill> sheetSkills = new ArrayList<>();
-        List<Skill> domainSkills = read("skills").from(sheet);
-        ReflectionReader readName = read("name");
-        ReflectionCaller callCost = call("cost");
-        ReflectionReader readCost = read("cost");
-        ReflectionReader readPageReference = read("pageReference");
-        ReflectionReader readModifiers = read("modifiers");
-        ReflectionReader readValue = read("value");
-        ReflectionReader readType = read("type");
-        ReflectionReader readControllingAttribute = read("controllingAttribute");
-        ReflectionReader readDifficultyLevel = read("difficultyLevel");
+        List<Advantage> domainAdvantages = readAdvantages.from(sheet);
+
         for (Advantage domainAdvantage : domainAdvantages) {
             SheetAdvantage sheetAdvantage = new SheetAdvantage(readName.from(domainAdvantage), callCost.of(domainAdvantage), readPageReference.from(domainAdvantage));
 
@@ -47,13 +61,21 @@ public class ReflectionConverter implements SheetConverter {
             }
             sheetAdvantages.add(sheetAdvantage);
         }
+        return sheetAdvantages;
+    }
+
+    private List<SheetSkill> convertSheetSkills() {
+        List<SheetSkill> sheetSkills = new ArrayList<>();
+        List<Skill> domainSkills = readSkills.from(sheet);
 
         for (Skill domainSkill : domainSkills) {
-            SheetSkill sheetSkill= new SheetSkill(readName.from(domainSkill), readCost.from(domainSkill), readPageReference.from(domainSkill), readControllingAttribute.from(domainSkill), readDifficultyLevel.from(domainSkill));
+            SheetSkill sheetSkill = new SheetSkill(readName.from(domainSkill), readCost.from(domainSkill), readPageReference.from(domainSkill), readControllingAttribute.from(domainSkill), readDifficultyLevel.from(domainSkill));
             sheetSkills.add(sheetSkill);
         }
+        return sheetSkills;
+    }
 
-
-        return new SheetSheet(metaData, sheetPoints, sheetAdvantages, sheetSkills, note);
+    private String convertNote() {
+        return readNote.from(sheet);
     }
 }
