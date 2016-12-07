8d5c6f2dfc0136ffa2c53b71bc4ee4eb107134d6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-07 14:25:33 +0100

    Assign Paul to issue 7

--- src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
+++ src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
@@ -5,6 +5,7 @@ import documentation.Developer;
 import org.gurpsdomain.domain.description.ModifierDescription;
 
 @Assign(developer= Developer.PAUL, issue = 21)
+@Assign(developer= Developer.PAUL, issue = 7)
 public interface ModifierDescriptionPredicate {
     boolean isFullfilledBy(ModifierDescription modifier);
 }
