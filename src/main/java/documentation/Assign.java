gurpsdomain/gurpsdomain

commit 8d5c6f2dfc0136ffa2c53b71bc4ee4eb107134d6

Assign Paul to issue 7

src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
@@ -5,6 +5,7 @@ import documentation.Developer;
 import org.gurpsdomain.domain.description.ModifierDescription;
 
 @Assign(developer= Developer.PAUL, issue = 21)
+@Assign(developer= Developer.PAUL, issue = 7)
 public interface ModifierDescriptionPredicate {
     boolean isFullfilledBy(ModifierDescription modifier);
 }

commit 2df7c1845d7e01377451e36d92bcb20468d37ff1

Fixed build break caused by previous commit 8d5c6f2dfc0136ffa2c53b71bc4ee4eb107134d6 duplicate use of annotation @Assign was not allowed. Solved this by expanding the interface to support an array of issue ids

src/main/java/documentation/Assign.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD,ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE, ElementType.TYPE_PARAMETER, })
 public @interface Assign {
     Developer developer();
-    int issue();
+    int[] issues();
 }

src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
@@ -4,8 +4,7 @@ import documentation.Assign;
 import documentation.Developer;
 import org.gurpsdomain.domain.description.ModifierDescription;
 
-@Assign(developer= Developer.PAUL, issue = 21)
-@Assign(developer= Developer.PAUL, issue = 7)
+@Assign(developer= Developer.PAUL, issues = {21,7})
 public interface ModifierDescriptionPredicate {
     boolean isFullfilledBy(ModifierDescription modifier);
 }