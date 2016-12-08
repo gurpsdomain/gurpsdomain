f111d36102abc2ada988868816ee73482bfc8243
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-08 20:47:15 +0100

    Allow multiple assignments

--- src/main/java/documentation/Assigns.java
+++ src/main/java/documentation/Assigns.java
@@ -1,11 +1,12 @@
 package documentation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Repeatable(value = Assigns.class)
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD,ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE, ElementType.TYPE_PARAMETER, })
-public @interface Assign {
-    Developer developer();
-    int[] issues();
+public @interface Assigns {
+    Assign[] value();
 }
--- src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
+++ src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
@@ -4,7 +4,8 @@ import documentation.Assign;
 import documentation.Developer;
 import org.gurpsdomain.domain.description.ModifierDescription;
 
-@Assign(developer= Developer.PAUL, issues = {21,7})
+@Assign(developer= Developer.PAUL, issues = {21, 7})
+@Assign(developer= Developer.DAAN, issues = {21, 7})
 public interface ModifierDescriptionPredicate {
     boolean isFulfilledBy(ModifierDescription modifier);
 }
