aa37f491afde153c0e9fc438f662ab6d964ba193
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2016-12-13 22:09:52 +0100

    Added AdvantageLevel to make levelable advantages

--- src/main/java/org/gurpsdomain/domain/AdvantageLevel.java
+++ src/main/java/org/gurpsdomain/domain/AdvantageLevel.java
@@ -1,3 +1,18 @@
 package org.gurpsdomain.domain;
 
-public class AdvantageLevel {}
+import org.gurpsdomain.domain.calc.AdvantageCostAccumulator;
+
+public class AdvantageLevel {
+    private final Cost cost;
+    public AdvantageLevel(Cost cost) {
+        this.cost = cost;
+    }
+
+    public Cost getCost() {
+        return cost;
+    }
+
+    public void accumulateCost(AdvantageCostAccumulator accumulator) {
+        cost.accumulateCost(accumulator);
+    }
+}
