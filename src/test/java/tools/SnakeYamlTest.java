18a971ed949671f70835f8b062a9513c529ede14
Marcel Gorissen <damnhighc4@hotmail.com>
2016-12-20 22:19:37 +0100

    Added SnakeYaml testing to learn for writing yamls

--- src/test/java/tools/SnakeYamlTest.java
+++ src/test/java/tools/SnakeYamlTest.java
@@ -1,13 +1,14 @@
 package tools;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
@@ -27,7 +28,29 @@ public class SnakeYamlTest {
         assertThat(data.get("library"), is("SnakeYaml"));
         assertThat(data, hasKey("usecases"));
         assertThat((List<String>) data.get("usecases"), contains("Parsing Yaml", "Dumping Yaml"));
-
     }
 
+    @Test
+    public void shouldProduceYamlFile() throws Throwable {
+        StringWriter writer = new StringWriter();
+        Yaml yaml = new Yaml();
+
+        Map<String, Object> data = new HashMap();
+        String[] usesCases = {"Parsing Yaml","Dumping Yaml"};
+        data.put("library","SnakeYaml");
+        data.put("usecases", usesCases);
+
+        yaml.dump(data, writer);
+        String output = writer.toString();
+
+
+        BufferedReader readFile = new BufferedReader(new FileReader(new File("src/test/resources/tools/snake.yml")));
+        String expectedOutput = "";
+        String line;
+        while((line = readFile.readLine()) != null){
+            expectedOutput += line;
+            expectedOutput += "\n";
+        }
+        assertThat(output,is(expectedOutput));
+    }
 }
