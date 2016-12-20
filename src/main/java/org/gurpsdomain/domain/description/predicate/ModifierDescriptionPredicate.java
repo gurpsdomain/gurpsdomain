3047f80002f512d86603975a7e7d73a13a078366
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-20 20:48:35 +0100

    Issue 21 is solved
    Leaving issue 7 for Paul

--- src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
+++ src/main/java/org/gurpsdomain/domain/description/predicate/ModifierDescriptionPredicate.java
@@ -4,8 +4,7 @@ import documentation.Assign;
 import documentation.Developer;
 import org.gurpsdomain.domain.description.ModifierDescription;
 
-@Assign(developer= Developer.PAUL, issues = {21, 7})
-@Assign(developer= Developer.DAAN, issues = {21, 7})
+@Assign(developer= Developer.PAUL, issues = {7})
 public interface ModifierDescriptionPredicate {
     boolean isFulfilledBy(ModifierDescription modifier);
 }
