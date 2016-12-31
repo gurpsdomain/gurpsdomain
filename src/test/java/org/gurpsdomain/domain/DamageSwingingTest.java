e94638651f2891a1310bc5529f2bd93d6d039787
Marcel Gorissen <damnhighc4@hotmail.com>
2016-12-31 15:37:14 +0100

    Added tests for swinging and thrusting damage

---
+++ src/test/java/org/gurpsdomain/domain/DamageSwingingTest.java
@@ -0,0 +1,35 @@
+package org.gurpsdomain.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DamageSwingingTest {
+    @Test
+    public void shouldDetermineCorrectDamageSwinging() {
+        Attributes attributes = new Attributes();
+
+        Dice damageSwinging = attributes.damageSwinging();
+
+        assertThat(damageSwinging.toString(), is("1d"));
+    }
+
+    @Test
+    public void shouldDetermineCorrectDamageSwingingAfterStrengthThreeBonus() {
+        Attributes attributes = new Attributes();
+        attributes.addStrengthBonus(3);
+        Dice damageSwinging = attributes.damageSwinging();
+
+        assertThat(damageSwinging.toString(), is("2d-1"));
+    }
+
+    @Test
+    public void shouldDetermineCorrectDamageSwingingAfterDamageSwingingBonus() {
+        Attributes attributes = new Attributes();
+        attributes.addDamageSwingingBonus(new Dice(1,3));
+        Dice damageSwinging = attributes.damageSwinging();
+
+        assertThat(damageSwinging.toString(), is("2d+3"));
+    }
+}
---
+++ src/test/java/org/gurpsdomain/domain/DamageThrustingTest.java
@@ -0,0 +1,35 @@
+package org.gurpsdomain.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DamageThrustingTest {
+    @Test
+    public void shouldDetermineCorrectDamageThrusting() {
+        Attributes attributes = new Attributes();
+
+        Dice damageThrusting = attributes.damageThrusting();
+
+        assertThat(damageThrusting.toString(), is("1d-2"));
+    }
+
+    @Test
+    public void shouldDetermineCorrectDamageThrustingAfterStrengthThreeBonus() {
+        Attributes attributes = new Attributes();
+        attributes.addStrengthBonus(3);
+        Dice damageThrusting = attributes.damageThrusting();
+
+        assertThat(damageThrusting.toString(), is("1d"));
+    }
+
+    @Test
+    public void shouldDetermineCorrectDamageThrustingAfterDamageThrustingBonus() {
+        Attributes attributes = new Attributes();
+        attributes.addDamageThrustingBonus(new Dice(1,3));
+        Dice damageThrusting = attributes.damageThrusting();
+
+        assertThat(damageThrusting.toString(), is("2d+1"));
+    }
+}
