9880c08b104ecbd89fa8094ee12860d7371b66ff
Marcel Gorissen <damnhighc4@hotmail.com>
2017-01-02 21:47:47 +0100

    renamed 'bonus' in Dice to 'adds', as used in the books

--- src/main/java/org/gurpsdomain/domain/Dice.java
+++ src/main/java/org/gurpsdomain/domain/Dice.java
@@ -3,26 +3,26 @@ package org.gurpsdomain.domain;
 
 public class Dice {
     private int number;
-    private int bonus;
+    private int adds;
 
     public static Dice sumDice(Dice firstDice, Dice secondDice) {
-        return new Dice(firstDice.number + secondDice.number, firstDice.bonus + secondDice.bonus);
+        return new Dice(firstDice.number + secondDice.number, firstDice.adds + secondDice.adds);
     }
 
-    public Dice(int number, int bonus) {
+    public Dice(int number, int adds) {
         if (number < 0) {
             throw new IllegalArgumentException("An negative number of " + number + " dice is unexpected.");
         }
         this.number = number;
-        this.bonus = bonus;
+        this.adds = adds;
     }
 
     public String toString() {
-        if (bonus < 0) {
-            return number + "d" + bonus;
+        if (adds < 0) {
+            return number + "d" + adds;
         }
-        if (bonus > 0) {
-            return number + "d+" + bonus;
+        if (adds > 0) {
+            return number + "d+" + adds;
         } else {
             return number + "d";
         }
