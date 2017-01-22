5b0e9e18dc1335507e86b329f0a6d2f286cc56e3
Marcel Gorissen <damnhighc4@hotmail.com>
2017-01-22 08:45:49 +0100

    Made DamageResistanceBonus support leveled

--- src/main/java/org/gurpsdomain/domain/DamageResistanceBonus.java
+++ src/main/java/org/gurpsdomain/domain/DamageResistanceBonus.java
@@ -2,18 +2,28 @@ package org.gurpsdomain.domain;
 
 public class DamageResistanceBonus {
     public static DamageResistanceBonus damageResistanceBonus(DamageResistance damageResistance, String bonus) {
-        return new DamageResistanceBonus(damageResistance, bonus);
+        return new DamageResistanceBonus(damageResistance, bonus, false);
+    }
+
+    public static DamageResistanceBonus LeveledDamageResistanceBonus(DamageResistance damageResistance, String bonus) {
+        return new DamageResistanceBonus(damageResistance, bonus, true);
     }
 
     private final DamageResistance damageResistance;
     private final String bonus;
+    private final boolean leveled;
 
-    private DamageResistanceBonus(DamageResistance damageResistance, String bonus) {
+    private DamageResistanceBonus(DamageResistance damageResistance, String bonus, boolean leveled) {
         this.damageResistance = damageResistance;
         this.bonus = bonus;
+        this.leveled = leveled;
     }
 
     public void applyTo(DamageResistances damageResistances) {
         damageResistance.addBonusTo(damageResistances, bonus);
+    }
+
+    public boolean isLeveled() {
+        return leveled;
     }
 }
