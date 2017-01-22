39513da67140793494b561c85d0d22d5c87f3eb2
Marcel Gorissen <damnhighc4@hotmail.com>
2017-01-22 07:43:41 +0100

    expanded DamageResistance(s) w/ missing data

--- src/main/java/org/gurpsdomain/domain/DamageResistance.java
+++ src/main/java/org/gurpsdomain/domain/DamageResistance.java
@@ -11,6 +11,78 @@ public enum DamageResistance {
         public void addBonusTo(DamageResistances damageResistances, String bonus) {
             damageResistances.addEyeBonus(Integer.parseInt(bonus));
         }
+    },
+    SKULL("Skull", "skull") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addSkullBonus(Integer.parseInt(bonus));
+        }
+    },
+    FACE("Face", "face") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addFaceBonus(Integer.parseInt(bonus));
+        }
+    },
+    RIGHT_LEG("Right Leg", "right leg", "R.Leg") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addRightLegBonus(Integer.parseInt(bonus));
+        }
+    },
+    RIGHT_ARM("Right Arm", "right arm", "R.Arm") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addRightArmBonus(Integer.parseInt(bonus));
+        }
+    },
+    TORSO("Torso", "torso") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addTorsoBonus(Integer.parseInt(bonus));
+        }
+    },
+    GROIN("Groin", "groin") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addGroinBonus(Integer.parseInt(bonus));
+        }
+    },
+    LEFT_ARM("Left Arm", "left arm", "L.Arm") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addLeftArmBonus(Integer.parseInt(bonus));
+        }
+    },
+    LEFT_LEG("Left Leg", "left leg", "L.Leg") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addLeftLegBonus(Integer.parseInt(bonus));
+        }
+    },
+    HAND("Hand", "hand") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addHandBonus(Integer.parseInt(bonus));
+        }
+    },
+    FOOT("Foot", "foot") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addFootBonus(Integer.parseInt(bonus));
+        }
+    },
+    NECK("Neck", "neck") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addNeckBonus(Integer.parseInt(bonus));
+        }
+    },
+    VITALS("Vitals", "vitals") {
+        @Override
+        public void addBonusTo(DamageResistances damageResistances, String bonus) {
+            damageResistances.addVitalsBonus(Integer.parseInt(bonus));
+        }
     };
 
     static final private Map<String, DamageResistance> descriptionToDamageResistance = new HashMap<>();
@@ -55,7 +127,9 @@ public enum DamageResistance {
         return defaultValue() + (int) bonus;
     }
 
-    protected int defaultValue(){return 0;}
+    protected int defaultValue() {
+        return 0;
+    }
 
     public abstract void addBonusTo(DamageResistances damageResistances, String bonus);
 }
--- src/main/java/org/gurpsdomain/domain/DamageResistances.java
+++ src/main/java/org/gurpsdomain/domain/DamageResistances.java
@@ -16,6 +16,54 @@ public class DamageResistances {
         addIntBonus(EYE, bonus);
     }
 
+    void addSkullBonus(int bonus) {
+        addIntBonus(SKULL, bonus);
+    }
+
+    void addFaceBonus(int bonus) {
+        addIntBonus(FACE, bonus);
+    }
+
+    void addRightLegBonus(int bonus) {
+        addIntBonus(RIGHT_LEG, bonus);
+    }
+
+    void addRightArmBonus(int bonus) {
+        addIntBonus(RIGHT_ARM, bonus);
+    }
+
+    void addTorsoBonus(int bonus) {
+        addIntBonus(TORSO, bonus);
+    }
+
+    void addGroinBonus(int bonus) {
+        addIntBonus(GROIN, bonus);
+    }
+
+    void addLeftArmBonus(int bonus) {
+        addIntBonus(LEFT_ARM, bonus);
+    }
+
+    void addLeftLegBonus(int bonus) {
+        addIntBonus(LEFT_LEG, bonus);
+    }
+
+    void addHandBonus(int bonus) {
+        addIntBonus(HAND, bonus);
+    }
+
+    void addFootBonus(int bonus) {
+        addIntBonus(FOOT, bonus);
+    }
+
+    void addNeckBonus(int bonus) {
+        addIntBonus(NECK, bonus);
+    }
+
+    void addVitalsBonus(int bonus) {
+        addIntBonus(VITALS, bonus);
+    }
+
     private void addIntBonus(DamageResistance damageResistance, int aBonus) {
         int previous = (int) bonuses.getOrDefault(damageResistance, damageResistance.defaultBonus());
         int current = previous + aBonus;
@@ -30,4 +78,51 @@ public class DamageResistances {
         return (int) value(EYE);
     }
 
+    int skull() {
+        return (int) value(SKULL);
+    }
+
+    int face() {
+        return (int) value(FACE);
+    }
+
+    int rightLeg() {
+        return (int) value(RIGHT_LEG);
+    }
+
+    int rightArm() {
+        return (int) value(RIGHT_ARM);
+    }
+
+    int torso() {
+        return (int) value(TORSO);
+    }
+
+    int groin() {
+        return (int) value(GROIN);
+    }
+
+    int leftARm() {
+        return (int) value(LEFT_ARM);
+    }
+
+    int leftLeg() {
+        return (int) value(LEFT_LEG);
+    }
+
+    int hand() {
+        return (int) value(HAND);
+    }
+
+    int foot() {
+        return (int) value(FOOT);
+    }
+
+    int neck() {
+        return (int) value(NECK);
+    }
+
+    int vitals() {
+        return (int) value(VITALS);
+    }
 }
